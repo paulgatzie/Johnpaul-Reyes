f8d81f4d7dabe08ba08c6c9ecb83363d23774368
paulgatzie <johnpaulgatziereyes@gmail.com>
2025-06-06 02:37:34 +0800

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { mochaPlugins } from "@getmocha/vite-plugins";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [...mochaPlugins(process.env), react()],
-  server: {
-    allowedHosts: true,
-  },
-  build: {
-    chunkSizeWarningLimit: 5000,
-  },
-});
+  plugins: [react()],
+  base: '/johnpaul-Reyes/'
+})
